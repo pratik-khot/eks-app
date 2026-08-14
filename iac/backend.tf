6da83303618793dfeeb9603c30595285b110d1eb
Pratik <khotpratik625@gmail.com>
2026-08-14 20:02:28 +0530

    changed backend

--- iac/backend.tf
+++ iac/backend.tf
@@ -1,8 +1,25 @@
+
+##For S3 as a backend
+# terraform {
+#   backend "s3" {
+#     bucket       = "my-state-bucket-339713094763-us-east-1-an"
+#     key          = "myapp/production/tfstate"
+#     region       = "us-east-1"
+#     use_lockfile = true
+#   }
+# }
+
+###For TFC as a Backend
+
 terraform {
-  backend "s3" {
-    bucket       = "my-state-bucket-339713094763-us-east-1-an"
-    key          = "myapp/production/tfstate"
-    region       = "us-east-1"
-    use_lockfile = true
+  required_version = "1.15.8"
+
+  cloud {
+    
+    organization = "tfc-pratik-khot"
+
+    workspaces {
+      name = "Dev"
+    }
   }
 }
